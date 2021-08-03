96b5a7e8a39e2311517cabecd3340901318c1146
gwarz <gmwarzecha@tutanota.com>
2021-08-03 07:06:04 -0700

    added transition to Toggle.js and cleaned up old code

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,33 +1,25 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useTransition, animated } from "react-spring";
 
 export const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
-  const { color, y } = useSpring({
-    // ternary operator sets the opacity in one line
-    // opacity: isToggled ? 1 : 0,
-    color: isToggled ? "tomato" : "green",
-    // fontSize: isToggled ? '2rem' : '5em',
-    y: isToggled ? 0 : 1,
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
   });
 
   return (
     <div>
-      <animated.h1
-        style={{
-          // y isn't a valid css property, need .interpolate to assign the value and return the property
-          transform: y
-            .interpolate({
-              // emulates css keyframes
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50],
-            })
-            .interpolate((y) => `translate3d(0, ${y}px, 0)`),
-          color,
-        }}
-      >
-        Howdy
-      </animated.h1>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.h1 key={key} style={props}>
+              Howdy
+            </animated.h1>
+          )
+      )}
+
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
     </div>
   );
