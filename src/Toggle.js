2f7812d8a8caa7dc75b102fb65f48015e574f5bc
gwarz <gmwarzecha@tutanota.com>
2021-08-09 07:24:38 -0700

    added router component with some page animations and transitions

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,8 +2,29 @@ import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
 
 export const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
+  const [items, setItems] = useState([
+    {
+      letter: "O",
+      key: 1,
+    },
+    {
+      letter: "l",
+      key: 2,
+    },
+    {
+      letter: "i",
+      key: 3,
+    },
+    {
+      letter: "v",
+      key: 4,
+    },
+    {
+      letter: "e",
+      key: 5,
+    },
+  ]);
+  const transition = useTransition(items, (item) => item.key, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
@@ -11,16 +32,44 @@ export const Toggle = () => {
 
   return (
     <div>
-      {transition.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Howdy
-            </animated.h1>
-          )
-      )}
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
 
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: "O",
+              key: 1,
+            },
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
+
+// const transition = useTransition(isToggled, null, {
+//   from: { opacity: 0, position: 'absolute' },
+//   enter: { opacity: 1 },
+//   leave: { opacity: 0 },
+// });
+
+// <div style={{
+//   position: 'relative'
+// }}>
+//   {transition.map(({ item, key, props }) =>
+//     item ? (
+//       <animated.h1 style={props}>Howdy</animated.h1>
+//     ) : (
+//       <animated.h1 style={props}>World</animated.h1>
+//     )
+//   )}
+
+//   <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+// </div>
